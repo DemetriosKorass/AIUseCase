624dfe0d98cdf0bce00844ee189dad57a4dde615
DmitryKrukovskyi <dkruk@softserveinc.com>
2023-06-25 17:43:54 +0300

    UC#10 Implement regex

---
+++ src/AIUseCase/RegexLib/StringValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexLib
+{
+    public class StringValidator
+    {
+        private const string SpecialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+        public bool IsStringValid(string input, int maxLength)
+        {
+            if (string.IsNullOrEmpty(input) || input.Length > maxLength)
+                return false;
+
+            string pattern = $"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[{SpecialCharacters}])[^\s]{{{maxLength}}}$";
+
+            return Regex.IsMatch(input, pattern);
+        }
+    }
+}
